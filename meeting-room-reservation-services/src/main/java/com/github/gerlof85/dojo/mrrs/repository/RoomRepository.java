a58e66008b2e21ab97b2f410aa8f3cf9528fcaa4
Gerlof <g.vriesinga@gmail.com>
2016-07-04 20:55:56 +0200

    Adds search function for room by location + getCapacity of a room

--- meeting-room-reservation-services/src/main/java/com/github/gerlof85/dojo/mrrs/repository/RoomRepository.java
+++ meeting-room-reservation-services/src/main/java/com/github/gerlof85/dojo/mrrs/repository/RoomRepository.java
@@ -1,6 +1,7 @@
 package com.github.gerlof85.dojo.mrrs.repository;
 
 import com.github.gerlof85.dojo.mrrs.domain.Room;
+import java.util.ArrayList;
 
 public class RoomRepository {
 
@@ -8,26 +9,48 @@ public class RoomRepository {
 	private String locatieReturn;
 	private int aantalKamers = 0;
 
+	ArrayList<Room> rooms = new ArrayList<Room>();
+	
 	public void add(Room room) {
-		this.room = room;
-		aantalKamers ++;
-		//System.out.println("Aantalkamers " + aantalKamers);
+		rooms.add( room );
 	}
 
 	public String getByLocation(String locatie) {
-		//Room locatie = room.getLocation();
 		//this.location = room.getLocation();
-		locatieReturn = "No room available with this location number";
 		
-		//System.out.println("room.getloc" + room.getLocation());
-		
-		for (int i = 0; i <= aantalKamers; i++) {
-			if (room.getLocation().contains(locatie)) {
+		locatieReturn = "No rooms available with this location number";
+				
+		//searchRoom(locatie);
+		for (Room r : rooms){
+			if(r.getLocation().contains(locatie)){
 				locatieReturn = locatie;
-			}
+			} 
 		}
 		
 		return locatieReturn;
 	}
 
+	public int getCapacity(String locatie) {
+		int capaciteit = 0;
+		
+		for (Room r : rooms){
+			if(r.getLocation().contains(locatie)){
+				capaciteit = r.getCapacity();
+			}
+		}
+		return capaciteit;
+	}
+
+	/*  old inefficient code
+		//this.room = room;
+		//aantalKamers ++; 
+	  
+	for (int i = 0; i <= aantalKamers; i++) {
+		if (room.getLocation().contains(locatie)) {
+			//zelfde vragen als bij getCapacity
+			locatieReturn = locatie;
+		}
+	}
+	return locatieReturn;		*/
+	
 }
--- meeting-room-reservation-services/src/test/java/com/github/gerlof85/dojo/mrrs/repository/RoomRepositoryTest.java
+++ meeting-room-reservation-services/src/test/java/com/github/gerlof85/dojo/mrrs/repository/RoomRepositoryTest.java
@@ -1,7 +1,6 @@
 package com.github.gerlof85.dojo.mrrs.repository;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 import com.github.gerlof85.dojo.mrrs.domain.Room;
@@ -11,18 +10,16 @@ public class RoomRepositoryTest {
 	@Test
 
 	public void create() throws Exception {
-
 		RoomRepository repo = new RoomRepository();
+		
 		repo.add(new Room("01.10", 12, "Zwölf"));
 		repo.add(new Room("01.12", 16, null));
-		//repo.add(new Room("01.14", 20, "  Dreizehn  "));
-		
-		
+		repo.add(new Room("01.14", 20, "  Dreizehn  "));
 		//Room room = repo.getByLocation("01.10");
-
+		
 		assertEquals("01.12", repo.getByLocation("01.12"));
-		//assertEquals(11, repo.getCapacity("01.12"));
-
+		assertEquals(20, repo.getCapacity("01.14"));
+		//assertEquals("Zwölf", repo.getByLocation("01.10")); getByName?
 	}
 
 }
